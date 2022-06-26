cef9d0ac7565790ba597740a2a6b48fbdc29fb1f
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-06-26 18:42:51 -0300

    feat: Implementando o metodo getByEmail

--- src/services/interfaces/IUserService.ts
+++ src/services/interfaces/IUserService.ts
@@ -2,6 +2,7 @@ import { UserDTO } from '../../models/interfaces';
 
 export default interface IUserService {
   getById(id: string): Promise<UserDTO>;
+  getByEmail(email: string): Promise<UserDTO>;
   create(user: UserDTO): Promise<string>;
   // create(user: UserDTO): Promise<UserDTO>;
   // updateById(id: string, user: UserDTO): Promise<UserDTO>;
