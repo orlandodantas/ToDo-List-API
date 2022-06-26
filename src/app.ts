b64a3c2ef18e68be948c5b39272a301f7020ad00
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-06-26 16:05:09 -0300

    feat: Implementando o endpoit /users

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import handleError from './middleware';
+import userRouter from './routers';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/users', userRouter);
 
 app.use(handleError);
 export default app;
