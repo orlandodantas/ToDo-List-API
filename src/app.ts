ba40d24b90eb29f6fa157e550305680578e1492a
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-06-26 18:41:43 -0300

    feat: Implementando o endpoint /auth

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import handleError from './middleware';
-import userRouter from './routers';
+import userRouter, { authRouter } from './routers';
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/users', userRouter);
+app.use('/users', userRouter).use('/auth', authRouter);
 
 app.use(handleError);
 export default app;
