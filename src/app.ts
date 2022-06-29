b939ead94c18f2c31776abae7db017a98cf82d85
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-06-29 19:06:16 -0300

    feat: Implementando o endpoint /tasks

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import handleError from './middleware';
-import userRouter, { authRouter } from './routers';
+import userRouter, { authRouter, taskRouter } from './routers';
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/users', userRouter).use('/auth', authRouter);
+app
+  .use('/users', userRouter)
+  .use('/auth', authRouter)
+  .use('/tasks', taskRouter);
 
 app.use(handleError);
 export default app;
