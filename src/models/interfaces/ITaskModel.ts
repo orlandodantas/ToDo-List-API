f0c850a7ae951b5aa5beeb827636d3e8c9cd1e36
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-06-29 10:00:41 -0300

    feat: Adicionando o metodo e refatorando os parametros de outro metodo

--- src/models/interfaces/ITaskModel.ts
+++ src/models/interfaces/ITaskModel.ts
@@ -18,9 +18,10 @@ export type TaskDTO = {
 export default interface ITaskModel {
   getAll(userId: string): Promise<TaskDTO[]>;
   getById(id: string, userId: string): Promise<TaskDTO>;
+  getAllDone(userId: string): Promise<TaskDTO[]>;
   create(task: TaskDTO, userId: string): Promise<TaskDTO>;
   updateDescriptionById(id: string, description: string): Promise<TaskDTO>;
   updateStatusById(id: string, status: StatusTask): Promise<TaskDTO>;
   deleteById(id: string): Promise<void>;
-  deleteAllDone(id: string): Promise<void>;
+  deleteAllDone(userId: string): Promise<void>;
 }
