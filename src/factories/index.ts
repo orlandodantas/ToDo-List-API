3d8df44a5e59396f70e5eba7568056278fcbcb98
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-06-29 12:09:40 -0300

    chore: Organizando os importes da pasta factory

--- src/factories/index.ts
+++ src/factories/index.ts
@@ -1,6 +1,7 @@
 import AuthFactory from './AuthFactory';
+import TaskFactory from './TaskFactory';
 import UserFactory from './UserFactory';
 
 export default UserFactory;
 
-export { AuthFactory };
+export { AuthFactory, TaskFactory };
