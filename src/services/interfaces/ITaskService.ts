bd3faa474ddb731b155489329dc9e550a507eebb
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-06-28 23:23:15 -0300

    feat: Criando e implementando a inteface ITaskService

---
+++ src/services/interfaces/ITaskService.ts
@@ -0,0 +1,11 @@
+import { StatusTask, TaskDTO } from '../../models/interfaces';
+
+export default interface ITaskService {
+  getAll(userId: string): Promise<TaskDTO[]>;
+  getById(id: string, userId: string): Promise<TaskDTO>;
+  create(task: TaskDTO, userId: string): Promise<TaskDTO>;
+  updateDescriptionById(id: string, description: string, userId: string): Promise<TaskDTO>;
+  updateStatusById(id: string, status: StatusTask, userId: string): Promise<TaskDTO>;
+  deleteById(id: string, userId: string): Promise<void>;
+  deleteAllDone(id: string, userId: string): Promise<void>;
+}
