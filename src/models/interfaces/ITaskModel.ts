a9d2e37b25a065928f31c42cb7a0f94a9f2ccf35
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-06-28 23:21:11 -0300

    feat: Criando e implementando a interface ITaskModel

---
+++ src/models/interfaces/ITaskModel.ts
@@ -0,0 +1,26 @@
+export enum StatusTask {
+  PENDENTE = 'PENDENTE',
+  ANDAMENTO = 'ANDAMENTO',
+  PRONTO = 'PRONTO',
+}
+
+export type TaskDTO = {
+  id?: string;
+  description: string;
+  status?: StatusTask;
+  createAt?: Date;
+  user?: {
+    name: string;
+    email: string;
+  };
+};
+
+export default interface ITaskModel {
+  getAll(userId: string): Promise<TaskDTO[]>;
+  getById(id: string, userId: string): Promise<TaskDTO>;
+  create(task: TaskDTO, userId: string): Promise<TaskDTO>;
+  updateDescriptionById(id: string, description: string): Promise<TaskDTO>;
+  updateStatusById(id: string, status: StatusTask): Promise<TaskDTO>;
+  deleteById(id: string): Promise<void>;
+  deleteAllDone(id: string): Promise<void>;
+}
